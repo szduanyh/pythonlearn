09edb990ac2203633405d3e7835ea143e39cfede
duanyh <duanyihua1234@gmail.com>
2015-12-02 22:39:03 +0800

    write a test script

---
+++ com/test/list/listtest.py
@@ -0,0 +1,11 @@
+__author__ = 'dyh'
+# -*- coding: utf-8 -*-
+
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+
+# print the length of list
+print(len(classmates))
+
+# 如果要取最后一个元素，除了计算索引位置外，还可以用-1做索引，直接获取最后一个元素：
+print(classmates[-1])
