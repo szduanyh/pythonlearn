ecf3e7b6a420715eed1079b9c7ad7cd615e5aa9f
duanyh <duanyihua1234@gmail.com>
2015-12-03 22:48:42 +0800

    write test

---
+++ com/test/deltest.py
@@ -0,0 +1,10 @@
+__author__ = 'dyh'
+# -*- coding: utf-8 -*-
+
+a = [-1, 2, 3, 4, 5, 666, 77, 123, 232.23]
+del a[0]
+
+print(a)
+
+del a[2:4]
+print(a)
---
+++ com/test/squares.py
@@ -0,0 +1,22 @@
+__author__ = 'dyh'
+# -*- coding: utf-8 -*-
+
+squares = [x ** 2 for x in range(10)]
+print(squares)
+
+L = [(x, y) for x in [1, 2, 3] for y in [2, 3, 4] if x != y]
+print(L)
+
+vec = [-4, -2, 0, 2, 4]
+
+# create a new list with the values double
+print([x * 2 for x in vec])
+
+# filter the list to exclude negative number
+print([x for x in vec if x >= 0])
+
+# applay a function to all elements
+print([abs(x) for x in vec])
+
+vec2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print([num for elem in vec2 for num in elem])
