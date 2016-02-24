34d61c4b6b0e32190c1de6e016f750acac1924f4
szlexun <duanyihua@lxyes.cn>
2016-02-24 15:04:53 +0800

    add

--- com/test/list/filetest.py
+++ com/test/list/filetest.py
@@ -4,10 +4,10 @@ __author__ = 'admin-1'
 
 # 打开一个文件
 fo = open("d:/foo.txt", "a+")
-print "Name of the file: ", fo.name
-print "Closed or not : ", fo.closed
-print "Opening mode : ", fo.mode
-print "Softspace flag : ", fo.softspace
+print("Name of the file: ", fo.name)
+print("Closed or not : ", fo.closed)
+print("Opening mode : ", fo.mode)
+print("Softspace flag : ", fo.softspace)
 
 fo.write("Python is a gread language!\nYeah,it is great!!!\n")
 fo.close()
